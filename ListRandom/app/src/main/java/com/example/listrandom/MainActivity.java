1c889e81e9e19b47d48d30a601504b18513afd55
skybluewild1 <151087545+skybluewild1@users.noreply.github.com>
2025-05-01 09:05:54 -0700

    list with add

--- ListRandom/app/src/main/java/com/example/listrandom/MainActivity.java
+++ ListRandom/app/src/main/java/com/example/listrandom/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listrandom;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,7 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ImageButton add_btn;
+    private EditText itemEdt;
+    private ArrayList<String> lngList;
     ListView listvew;
     String[] homework = {"Algorithms",
             "Data Structures",
@@ -31,12 +40,36 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         listvew = findViewById(R.id.listvew);
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,homework);
+        add_btn = findViewById(R.id.btn_add);
+        itemEdt = findViewById(R.id.idEdtItemName);
+        lngList = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,lngList);
         listvew.setAdapter(adapter);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           // on below line we are getting text from edit text
+                                           String item = itemEdt.getText().toString();
+
+                                           // on below line we are checking if item is not empty
+                                           if (!item.isEmpty()) {
+
+                                               // on below line we are adding item to our list.
+                                               lngList.add(item);
+
+                                               // on below line we are notifying adapter
+                                               // that data in list is updated to
+                                               // update our list view.
+                                               adapter.notifyDataSetChanged();
+                                           }
+
+                                       }
+                                   });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
 
     }
